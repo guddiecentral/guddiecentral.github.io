1a199e3890f7224e5bccd829bb6f2033d4d8d007
Max Parry <66670928+Buggem@users.noreply.github.com>
2024-01-20 17:15:42 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+var notificationBtn = document.getElementById("sub");
+
+
+
+function handlePermission(permission) {
+    // set the button to shown or hidden, depending on what the user answers
+    notificationBtn.style.display =
+      Notification.permission === "granted" ? "none" : "block";
+}
+
+function askNotificationPermission() {
+  // function to actually ask the permissions
+  
+
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support notifications.");
+  } else {
+    Notification.requestPermission().then((permission) => {
+      handlePermission(permission);
+      const notification = new Notification("Guddie Central", { body: "Thank you! We will be sure to notify you when we are finished." });
+
+    });
+  }
+}
